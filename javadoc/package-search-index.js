cf1adb078ea2053844c18e37ffd940835afb517c
zacharias <zacharias@4zellen.se>
2022-08-10 14:53:02 +0200

    trying to put javadoc in github pages

---
+++ javadoc/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"me.alien.networking.client"},{"l":"me.alien.networking.server"},{"l":"me.alien.networking.util"},{"l":"me.alien.networking.util.packages"}];updateSearchResults();
